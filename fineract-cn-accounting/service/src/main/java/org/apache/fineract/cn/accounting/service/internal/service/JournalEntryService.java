1c11e30f81ffbc3f17f0733d946985dc62aa69f2
Padmaraju <padmaraju.sattineni@finterra.org>
2019-03-10 17:28:10 +0800

    Journal entry reverse order

--- fineract-cn-accounting/service/src/main/java/org/apache/fineract/cn/accounting/service/internal/service/JournalEntryService.java
+++ fineract-cn-accounting/service/src/main/java/org/apache/fineract/cn/accounting/service/internal/service/JournalEntryService.java
@@ -80,7 +80,7 @@ public class JournalEntryService {
                           )
                   ) == 0
               )
-              .sorted(Comparator.comparing(JournalEntryEntity::getTransactionDate))
+              .sorted(Comparator.comparing(JournalEntryEntity::getTransactionDate).reversed())
               .collect(Collectors.toList());
 
       final List<TransactionTypeEntity> transactionTypes = this.transactionTypeRepository.findAll();
