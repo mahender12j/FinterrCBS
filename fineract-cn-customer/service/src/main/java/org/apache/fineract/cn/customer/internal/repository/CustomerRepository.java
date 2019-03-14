fc4a46b51f06dc6631988a30d16fb5272f5dbbbe
Robin HasSan <robiul.hassan12102@gmail.com>
2019-03-15 01:30:38 +0800

    FIX refferal code

--- fineract-cn-customer/service/src/main/java/org/apache/fineract/cn/customer/internal/repository/CustomerRepository.java
+++ fineract-cn-customer/service/src/main/java/org/apache/fineract/cn/customer/internal/repository/CustomerRepository.java
@@ -35,7 +35,7 @@ public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> 
     Boolean existsByIdentifier(@Param("identifier") final String identifier);
 
 
-    @Query("select COUNT(c) from  CustomerEntity c where c.refferalCodeIdentifier=:identifier")
+    @Query("select COUNT(c) from  CustomerEntity c where c.referenceCustomer=:identifier")
     Integer findAllByRefferalCodeIdentifier(@Param("identifier") final String identifier);
 
     Page<CustomerEntity> findByIdentifierContainingOrGivenNameContainingOrSurnameContaining(
