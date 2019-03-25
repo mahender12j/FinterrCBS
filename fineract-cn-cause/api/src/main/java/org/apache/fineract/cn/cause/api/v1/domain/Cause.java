3a024f96acb6e7fab89176d8de5d2ebcc3e07d87
Josef <josefwijaya@outlook.com>
2019-03-25 11:04:24 +0800

    Update Cause - only allow certain cause state to be deleted

--- fineract-cn-cause/api/src/main/java/org/apache/fineract/cn/cause/api/v1/domain/Cause.java
+++ fineract-cn-cause/api/src/main/java/org/apache/fineract/cn/cause/api/v1/domain/Cause.java
@@ -33,6 +33,10 @@ public class Cause {
         PENDING, ACTIVE, LOCKED, REJECTED, CLOSED, DELETED
     }
 
+    public enum RemovableCauseState {
+        APPROVED, PENDING, REJECTED, INACTIVE
+    }
+
     @NotBlank
     private String identifier;
     @NotBlank
