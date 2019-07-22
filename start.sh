767096126b7a31a2565630e82d0a488edc49dd02
Md Robiul Hassan <robiul.hassan@finterra.org>
2019-07-22 16:08:52 +0800

    midified state.sh

--- start.sh
+++ start.sh
@@ -1,39 +1,41 @@
 
 git checkout staging;
 
-echo "checkout to staging";
+echo "######################## Checkout to staging ########################";
 
 git merge dev;
 
 
-echo "merge to dev";
+echo "######################## Merge to dev ########################";
 
 git push origin staging;
 
-echo "push to staging";
+echo "######################## Push to staging ########################";
 
 
 ssh ubuntu@3.113.55.81 -i Fineract-Satging.pem << EOF
 
-echo "SSH access granted to staging ubuntu";
+echo "######################## SSH access granted to staging ubuntu ########################";
 
 cd FinterraCBS;
 
-echo "CD to root";
+echo "######################## CD to root ########################";
 
- git pull ssh staging;
+git pull ssh staging;
 
-echo "PULL Staging from the server";
+echo "######################## PULL Staging from the server ########################";
 
-# cd fineract-cn-cause;
-# bash r.sh;
+cd fineract-cn-cause;
+bash r.sh;
 
-# cd fineract-cn-customer;
-# bash r.sh;
+cd fineract-cn-customer;
+bash r.sh;
 
-# sudo docker-compose down; sudo docker-compose up -d --build;
+sudo docker-compose down; sudo docker-compose up -d --build;
+
+echo "######################## BUILD FINISH FOR SERVICES ########################";
 
 EOF
 
-echo "checkout back to dev";
+echo "######################## checkout back to dev ########################";
 git checkout dev;
