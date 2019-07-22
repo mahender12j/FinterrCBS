784b6bb37d846a214ec268e4aa0ef462ed3bbb54
Md Robiul Hassan <robiul.hassan@finterra.org>
2019-07-22 15:49:56 +0800

    update auto deployment

--- start.sh
+++ start.sh
@@ -1,1 +1,16 @@
+ssh ubuntu@3.113.55.81 -i ~/Downloads/Fineract-Satging.pem << EOF
+cd FinterraCBS;
+
+git pull;
+
+cd fineract-cn-cause;
+bash r.sh;
+
+cd fineract-cn-customer;
+bash r.sh;
+
 sudo docker-compose down; sudo docker-compose up -d --build;
+
+EOF
+
+
