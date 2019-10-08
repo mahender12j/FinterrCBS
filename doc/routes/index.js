79302a6dc5460b9eb6f03b30f6ca2cbd6fcc525b
Md Robiul Hassan <robiul.hassan@finterra.org>
2019-10-08 17:52:17 +0800

    fix git diff issue

--- doc/routes/index.js
+++ doc/routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var cmd = require('node-cmd');
+var obj = JSON.parse(fs.readFileSync('./all-log.json', 'utf8'));
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var obj = JSON.parse(fs.readFileSync('./all-log.json', 'utf8'));
   res.render('index', { title: 'GIT COMMIT', obj: obj });
 });
 
@@ -13,17 +13,45 @@ router.get('/', function (req, res, next) {
 /* GET commit details. */
 router.get('/:id', function (req, res, next) {
   const queryParam = req.params.id;
-  const command =
-    `
+  // git rev-list --parents -n 1 48afa4ef76855468206510085930c0005495cec9
+
+  const commit_ids_commands = `
+  cd ..
+  git rev-list --parents -n 1 ${queryParam}
+  `;
+
+  // console.log(commit_ids_commands);
+
+  cmd.get(commit_ids_commands, (err_id, data_id, stderr_id) => {
+    // console.log('err----', err_id);
+    // console.log('data----', data_id);
+    // console.log('stderr----', err_id);
+
+
+    const diff_command = `git diff ${data_id} --color-words | aha > doc/files/${queryParam}.html`
+
+    const command =
+      `
     cd ..
-    git diff ${queryParam} --color-words | aha > doc/files/${queryParam}.html
+    ${diff_command.replace(/(\r\n|\n|\r)/gm, "")}
     `;
 
-  cmd.get(command, (err, data, stderr) => {
-    const redirectUrl = `static/${queryParam}.html`
-    // res.render('commit', { title: 'view commit', param: req.params.id });
-    res.redirect(redirectUrl);
+    // console.log('command------------------------: ',);
+
+    cmd.get(command, (err, data, stderr) => {
+      // console.log('data-------->', data);
+      const redirectUrl = `static/${queryParam}.html`
+      // res.render('commit', { title: 'view commit', param: req.params.id });
+      res.redirect(redirectUrl);
+    });
+
   });
+
+  // cmd.get(command, (err, data, stderr) => {
+  //   const redirectUrl = `static/${queryParam}.html`
+  //   // res.render('commit', { title: 'view commit', param: req.params.id });
+  //   res.redirect(redirectUrl);
+  // });
 });
 
 module.exports = router;
