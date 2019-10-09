0e1681329b8022be5ca26ebb043978b0b0b7464f
Md Robiul Hassan <robiul.hassan@finterra.org>
2019-10-09 12:01:07 +0800

    added pages and routes

--- doc/routes/index.js
+++ doc/routes/index.js
@@ -11,47 +11,64 @@ router.get('/', function (req, res, next) {
 
 
 /* GET commit details. */
-router.get('/:id', function (req, res, next) {
+router.get('/commit/:id', function (req, res, next) {
   const queryParam = req.params.id;
   // git rev-list --parents -n 1 48afa4ef76855468206510085930c0005495cec9
-
   const commit_ids_commands = `
   cd ..
   git rev-list --parents -n 1 ${queryParam}
   `;
-
-  // console.log(commit_ids_commands);
-
   cmd.get(commit_ids_commands, (err_id, data_id, stderr_id) => {
-    // console.log('err----', err_id);
-    // console.log('data----', data_id);
-    // console.log('stderr----', err_id);
-
-
     const diff_command = `git diff ${data_id} --color-words | aha > doc/files/${queryParam}.html`
-
     const command =
       `
     cd ..
     ${diff_command.replace(/(\r\n|\n|\r)/gm, "")}
     `;
-
-    // console.log('command------------------------: ',);
-
     cmd.get(command, (err, data, stderr) => {
-      // console.log('data-------->', data);
-      const redirectUrl = `static/${queryParam}.html`
-      // res.render('commit', { title: 'view commit', param: req.params.id });
+      const redirectUrl = `/static/${queryParam}.html`
       res.redirect(redirectUrl);
     });
-
   });
-
-  // cmd.get(command, (err, data, stderr) => {
-  //   const redirectUrl = `static/${queryParam}.html`
-  //   // res.render('commit', { title: 'view commit', param: req.params.id });
-  //   res.redirect(redirectUrl);
-  // });
 });
+
+
+/* GET documentation page. */
+router.get('/doc', function (req, res, next) {
+  res.render('doc', { title: 'Documentation'});
+});
+
+
+/* GET about page. */
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About the system'});
+});
+
+/* GET about page. */
+router.get('/diagram', function (req, res, next) {
+  res.render('diagram', { title: 'Microservices Diagram'});
+});
+
+/* GET identity-ms page. */
+router.get('/microservices/identity-ms', function (req, res, next) {
+  res.render('identity-ms', { title: 'Identity-ms'});
+});
+/* GET cause-ms page. */
+router.get('/microservices/cause-ms', function (req, res, next) {
+  res.render('cause-ms', { title: 'Cause-ms'});
+});
+/* GET customer-ms page. */
+router.get('/microservices/customer-ms', function (req, res, next) {
+  res.render('customer-ms', { title: 'Customer-ms'});
+});
+/* GET ledger-ms page. */
+router.get('/microservices/ledger-ms', function (req, res, next) {
+  res.render('ledger-ms', { title: 'Ledger-ms/Accounting-ms'});
+});
+/* GET rhythm-ms page. */
+router.get('/microservices/rhythm-ms', function (req, res, next) {
+  res.render('rhythm-ms', { title: 'Rhythm-ms'});
+});
+
 
 module.exports = router;
